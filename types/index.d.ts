4896f4e209a2225b34dd7121f20ed93d272e6712
Talon <wettsteintalon9@gmail.com>
2025-04-12 09:14:10 -0700

    Fix Typing Errors

--- types/index.d.ts
+++ types/index.d.ts
@@ -5,6 +5,6 @@ import * as GLTypes from "./gl";
 import { Calls as calls } from "./calls";
 import { Constants } from "./constants";
 
-export interface Calls extends calls<GLTypes.GLenum, GLTypes.GLboolean, GLTypes.GLbitfield, GLvoid, GLbyte, GLshort, GLTypes.GLint, GLubyte, GLushort, GLTypes.GLuint, GLTypes.GLsizei, GLTypes.GLfloat, GLTypes.GLclampf, GLTypes.GLdouble, GLTypes.GLclampd> {}
+export interface Calls extends calls<GLTypes.GLenum, GLTypes.GLboolean, GLTypes.GLbitfield, GLTypes.GLvoid, GLTypes.GLbyte, GLTypes.GLshort, GLTypes.GLint, GLTypes.GLubyte, GLTypes.GLushort, GLTypes.GLuint, GLTypes.GLsizei, GLTypes.GLfloat, GLTypes.GLclampf, GLTypes.GLdouble, GLTypes.GLclampd> {}
 
 export interface ExportValue extends Calls,Constants {}
